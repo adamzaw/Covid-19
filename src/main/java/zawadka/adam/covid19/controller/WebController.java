da2a4d23534f9197ddd3d33242655478220efcf1
Adam Zawadka <zawadka.adam@gmail.com>
2020-03-26 21:22:16 +0100

    www and Json

--- src/main/java/zawadka/adam/covid19/controller/WebController.java
+++ src/main/java/zawadka/adam/covid19/controller/WebController.java
@@ -1,14 +1,27 @@
 package zawadka.adam.covid19.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import zawadka.adam.covid19.service.DataRequest;
 
 @Controller
 public class WebController {
-@GetMapping("/www/")
-public String home(){
 
-    return "home";
-}
+    DataRequest dataRequest = new DataRequest();
+
+    @GetMapping("/www/total/")
+    public String home(ModelMap map) throws JsonProcessingException {
+
+        map.put("total",dataRequest.getTotal());
+        return "home";
+    }
+    @GetMapping("/www/list/")
+    public String list(ModelMap map) throws JsonProcessingException {
+
+        map.put("list",dataRequest.getDataByCoutry());
+        return "list";
+    }
 
 }
